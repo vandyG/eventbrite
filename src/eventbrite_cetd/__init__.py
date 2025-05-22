8d3d73bb41983b1c4acb9204a40e4f59b345634b
Vandit Goel <vandy.goel23@gmail.com>
2025-05-22 13:23:42 -0500

    build: update imports

--- src/eventbrite_cetd/__init__.py
+++ src/eventbrite_cetd/__init__.py
@@ -5,6 +5,6 @@ Fetch Event Details from Eventbrite.
 
 from __future__ import annotations
 
-from eventbrite_cetd._internal.cli import get_parser, main
+from eventbrite_cetd._internal.cli import run
 
-__all__: list[str] = ["get_parser", "main"]
+__all__: list[str] = ["run"]
