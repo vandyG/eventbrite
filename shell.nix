9c62ae2d581232afcb8f04e685445bb0a89a97b7
Vandit Goel <vandy.goel23@gmail.com>
2025-05-23 11:56:20 -0500

    chore: add env vars for python installations

--- shell.nix
+++ shell.nix
@@ -1,43 +1,71 @@
 let
   # Stable and unstable nixpkgs
-  stablePkgs = import <nixpkgs> {};
+  stablePkgs = import <nixpkgs> { };
   unstablePkgs = import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
-  }) {};
+  }) { };
 
   inherit (stablePkgs) lib;
 
-  pyproject-nix = import (builtins.fetchGit {
-    url = "https://github.com/pyproject-nix/pyproject.nix.git";
-  }) {
-    inherit lib;
-  };
+  # libraryPath = with stablePkgs; lib.makeLibraryPath [ stdenv.cc.cc.lib ];
 
-  uv2nix = import (builtins.fetchGit {
-    url = "https://github.com/pyproject-nix/uv2nix.git";
-  }) {
-    inherit pyproject-nix lib;
-  };
+  pyproject-nix =
+    import
+      (builtins.fetchGit {
+        url = "https://github.com/pyproject-nix/pyproject.nix.git";
+      })
+      {
+        inherit lib;
+      };
 
-  pyproject-build-systems = import (builtins.fetchGit {
-    url = "https://github.com/pyproject-nix/build-system-pkgs.git";
-  }) {
-    inherit pyproject-nix uv2nix lib;
-  };
+  uv2nix =
+    import
+      (builtins.fetchGit {
+        url = "https://github.com/pyproject-nix/uv2nix.git";
+      })
+      {
+        inherit pyproject-nix lib;
+      };
 
-in stablePkgs.mkShell {
+  pyproject-build-systems =
+    import
+      (builtins.fetchGit {
+        url = "https://github.com/pyproject-nix/build-system-pkgs.git";
+      })
+      {
+        inherit pyproject-nix uv2nix lib;
+      };
+
+in
+stablePkgs.mkShell {
+
   packages = with stablePkgs; [
-    python310
-    python311
-    python312
-    python313
+    (python311.withPackages (python-pkgs: with python-pkgs; [
+      python
+      numpy_2
+    ]))
+    (python312.withPackages (python-pkgs: with python-pkgs; [
+      python
+      numpy_2
+    ]))
+    (unstablePkgs.python313.withPackages (python-pkgs: with python-pkgs; [
+      python
+      numpy_2
+    ]))
     unstablePkgs.uv
     unstablePkgs.ruff
+    nil
+    nixfmt-rfc-style
   ];
+
+  # export LD_LIBRARY_PATH=${libraryPath}:$LD_LIBRARY_PATHx
 
   shellHook = ''
     export TMPDIR=/tmp
     export RUFF_PATH=$(which ruff)
+    export PYTHON311=$(which python3.11)
+    export PYTHON312=$(which python3.12)
+    export PYTHON313=$(which python3.13)
     echo "Shell with multiple Python versions, uv (from unstable), and uv2nix tooling."
   '';
 }
